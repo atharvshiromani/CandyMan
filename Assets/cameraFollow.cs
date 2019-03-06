f10b93a8a1ca0fa30378500bead64654b78cac79
Shubham <skathuria@ufl.edu>
2019-03-05 20:08:03 -0500

    player movement corrected

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+    
+{ private Vector2 velocity;
+    public GameObject[] player;
+    public float smoothTimeY;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+    public bool bounds;
+    private float smoothTimeX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // float posX = Mathf.SmoothDamp(transform.position.x, player.tranform.position.x, ref velocity.x, smoothTieX);
+        float posX = Mathf.SmoothDamp(transform.position.x, transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, transform.position.y, ref velocity.y, smoothTimeY);
+        transform.position = new Vector3(posX, posY, transform.position.z);
+        if(bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                 Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Animator anim;
+    private SpriteRenderer sr;
+    private float speed = 3f;
+    // Start is called before the first frame update
+   
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+    private void move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector3 temp = transform.position;
+        if(h > 0)
+        {
+            temp.x += speed * Time.deltaTime;
+            sr.flipX = false;
+        } else if (h < 0)
+        {
+            temp.x -= speed * Time.deltaTime;
+            sr.flipX = true;
+
+        } else if (h == 0)
+        {
+
+        }
+        transform.position = temp;
+    }
+}
