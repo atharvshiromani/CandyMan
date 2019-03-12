874ebb8409dd2bf6cd1cedc540c07a32bdd7fa80
Shubham <skathuria@ufl.edu>
2019-03-12 12:42:38 -0400

    Game Menu

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
