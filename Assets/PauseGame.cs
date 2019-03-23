32424ad8aa5864df5a1155ecbc992f9889ee8ebe
atharvshiromani <47363273+atharvshiromani@users.noreply.github.com>
2019-03-23 01:21:44 -0400

    Cinemachine
    the camera now follows wherever the player goes

--- Assets/PauseGame.cs
+++ Assets/PauseGame.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
-
-    // Update is called once per frame
+    public static bool GamePaused = false;
+    public GameObject pauseGame1;
+    
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseGame1.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+    void Pause()
+    {
+        pauseGame1.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
     }
 }
