232f0ae76ddf660c50dce6d66ce171fd522f3351
Shubham <skathuria@ufl.edu>
2019-04-15 04:17:23 -0400

    cool stuff

---
+++ Assets/change.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class change : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("scoreBoard", 3);
+    }
+
+    void scoreBoard()
+    {
+        SceneManager.LoadScene(newLevel);
+
+    }
+}
--- Assets/newlevl.cs
+++ Assets/newlevl.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class newlevl : MonoBehaviour
 {
     [SerializeField] private string newLevel;
+    [SerializeField] private string scoreboard;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(newLevel);
+            SceneManager.LoadScene(scoreboard);
+            Invoke("scoreBoard", 5);
         }
+    }
+
+    private void scoreBoard()
+    {
+        SceneManager.LoadScene(newLevel);
     }
 }
