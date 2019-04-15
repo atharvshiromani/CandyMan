075c0c170aa0ff490eaf5431ae2ed548d28b9c26
Shubham <skathuria@ufl.edu>
2019-04-15 11:07:03 -0400

    pause menu added

---
+++ Assets/pauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    [SerializeField] private string mainmenu;
+
+    public GameObject pausemenu, resumemenu;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+           
+        }
+
+    }
+
+   public void Pause()
+    {
+        pausemenu.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+   public void Resume()
+    {
+        pausemenu.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+    public void mainMenu()
+    {
+        SceneManager.LoadScene(mainmenu);
+        Time.timeScale = 1;
+
+    }
+}
