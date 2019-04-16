cebbecfb773cae9db8e13ad2b6cb255190761db4
Shubham <skathuria@ufl.edu>
2019-04-15 21:58:19 -0400

    update level 3

--- Assets/levelcomplete.cs
+++ Assets/levelcomplete.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class levelcomplete : MonoBehaviour
 {
+    public GameObject lvlcomplete;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
+
     }
 
     // Update is called once per frame
